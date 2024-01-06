cf0376027fb47193e8b0c62c00f508e61c4c3441
Avijit Dey <deyavijit134@gmail.com>
2024-01-06 20:47:35 +0530

    Add : Checkout Page Login And Coupon Form

---
+++ Js/chekout.js
@@ -0,0 +1,19 @@
+let login = document.querySelector(".visible-login");
+login.addEventListener("click", () => {
+  let loginForm = document.querySelector(".login-form");
+  if (loginForm.style.display === "block") {
+    loginForm.style.display = "none";
+  } else {
+    loginForm.style.display = "block";
+  }
+});
+
+let coupon = document.querySelector(".visible-coupon");
+coupon.addEventListener("click", () => {
+  let couponForm = document.querySelector(".coupon-form");
+  if (couponForm.style.display === "block") {
+    couponForm.style.display = "none";
+  } else {
+    couponForm.style.display = "block";
+  }
+});
